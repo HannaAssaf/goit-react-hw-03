b42d9611e0a6bde64636d53e935e5359999522b1
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-05-20 21:53:23 +0100

    input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,39 @@
+import { Formik, Form, Field } from "formik";
+import { useId } from "react";
 import css from "./ContactForm.module.css";
 
 export default function ContactForm() {
-  //   return (
+  const fieldId = useId();
+
+  return (
+    <Formik
+      inicialContacts={{
+        username: "",
+        email: "",
+      }}
+      onSubmit={() => {}}
+    >
+      <Form className={css.form}>
+        <Field
+          type="text"
+          name="username"
+          className={css.field}
+          id={`${fieldId}-username`}
+        />
+        <Field
+          type="tel"
+          name="tel"
+          className={css.field}
+          id={`${fieldId}-tel`}
+        />
+      </Form>
+    </Formik>
+  );
+
   // <form className={css.form} onSubmit={handleSubmit}>
   //   <input className={css.field} type="text" name="text" />
   //   <input className={css.field} type="text" name="number" />
   //   <button type="submit">Add contact</button>
   // </form>
-  //   );
+  //
 }
