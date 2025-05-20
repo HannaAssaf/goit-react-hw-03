05b61139b8be3187b8cc7ae2505d3667d4066db3
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-05-20 01:43:16 +0100

    task

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import css from "./App.module.css";
+import contactsBase from "../data/contacts.json";
+import ContactList from "../components/ContactList/ContactList";
+import SearchBox from "../components/SearchBox/SearchBox";
+import ContactForm from "../components/ContactForm/ContactForm";
+
+function App() {
+  const [contacts, setContacts] = useState(contactsBase);
+
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList contacts={contacts} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import css from "./Contact.module.css";
+export default function Contact({ data: { id, name, number }, onDelete }) {
+  return (
+    <div>
+      <div>
+        <p>{name}</p>
+        <p>{number}</p>
+      </div>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </div>
+  );
+}
