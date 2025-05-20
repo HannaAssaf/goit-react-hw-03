4a8ce0c1aa54e51cfd2a9e3a4be0b65e9485f51c
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-05-20 19:00:06 +0100

    search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { useDebounce } from "use-debounce";
 import css from "./App.module.css";
-import contactsBase from "../data/contacts.json";
+import inicialContacts from "../data/contacts.json";
 import ContactList from "../components/ContactList/ContactList";
 import SearchBox from "../components/SearchBox/SearchBox";
 import ContactForm from "../components/ContactForm/ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState(contactsBase);
+  const [contacts, setContacts] = useState(inicialContacts);
+  const [inputValue, setInputValue] = useState("");
+  const [debouncedInputValue] = useDebounce(inputValue, 300);
+
+  const contactsFilter = useMemo(() => {
+    // if (!debouncedInputValue.trim()) return contacts;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(debouncedInputValue.toLowerCase())
+    );
+  }, [debouncedInputValue, contacts]);
+
+  const handleDelete = (id) =>
+    setContacts((prev) => prev.filter((c) => c.id !== id));
 
   return (
     <>
       <div className={css.container}>
         <h1>Phonebook</h1>
         <ContactForm />
-        <SearchBox />
-        <ContactList contacts={contacts} />
+        <SearchBox text={inputValue} onChange={setInputValue} />
+        <ContactList contacts={contactsFilter} onDelete={handleDelete} />
       </div>
     </>
   );
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,1 +1,17 @@
-export default function SearchBox() {}
+import css from "./SearchBox.module.css";
+
+export default function SearchBox({ text, onChange }) {
+  return (
+    <div>
+      <label className={css.label}>
+        Find contacts by name
+        <input
+          className={css.search}
+          type="text"
+          value={text}
+          onChange={(ev) => onChange(ev.target.value)}
+        />
+      </label>
+    </div>
+  );
+}
